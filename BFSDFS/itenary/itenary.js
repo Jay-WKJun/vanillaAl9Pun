6d5e832bfa878c4aa04688c43a58dca636618d00
ha-young <hayeong28@gmail.com>
2021-02-14 17:21:35 +0900

    itenary done

--- BFSDFS/itenary/itenary.js
+++ BFSDFS/itenary/itenary.js
@@ -13,12 +13,114 @@
     모든 도시를 방문할 수 없는 경우는 주어지지 않습니다.
 */
 const tickets1 = [["ICN", "JFK"], ['HND', 'IAD'], ['JFK', 'HND']];
-
 const tickets2 = [['ICN', 'SFO'], ['ICN', 'ATL'], ['SFO', 'ATL'], ['ATL', 'ICN'], ['ATL','SFO']];
+const tickets3 = [['ICN', 'A'], ['ICN', 'B'], ['B', 'ICN']];
+const tickets4 = [['ICN', 'A'],['ICN', 'A'],['ICN', 'A'],['A', 'ICN'],['A', 'ICN']];
+const tickets5 = [['ICN','B'],['B','ICN'],['ICN','A'],['A','D'],['D','A']];
 
 // 출발지가 여러개 일 때, 뒤의 알파벳을 따져서 가야함.
 
 function solution(tickets) {
+  let answer = [];
+  const ticketsMap = ticketsToMap(tickets);
+
+  let departure = 'ICN';
+  answer = itenaryDFS(ticketsMap, departure, answer);
+
+  return answer;
+}
+
+function itenaryDFS(ticketsMap, departure) {
+  // 뺏을때 맵이 아주 비어있으면 정답이다.
+  // 끝가지 가서 undefined가 아닌게 정답이므로 마지막에 정답판정나면 하나씩 가지고올라온다.
+  let returnResult = [];
+
+  // 다 되었다면...? 
+  if (ticketsMap.size === 0) {
+    return [departure];
+  }
+
+  // 없다...? 이건 잘못된 경로 -> undefined리턴
+  if (!ticketsMap.has(departure)) {
+    return;
+  }
+
+  const arrives = ticketsMap.get(departure);
+  let index = 0;
+  while (arrives.length > 0) {
+    const currentArrive = arrives.splice(index, 1)[0];
+    index++;
+    const ticketsCopy = new Map();
+    for (let [key, value] of ticketsMap) {
+      if (value.length !== 0) {
+        ticketsCopy.set(key, [...value]);
+      }
+    }
+    returnResult = itenaryDFS(ticketsCopy, currentArrive);
+    if (returnResult) {
+      returnResult.unshift(departure);
+      break;
+    } else if (arrives.length === 0) {
+      return;
+    } else {
+      arrives.push(currentArrive);
+      index = 0;
+    }
+  }
+
+  return returnResult;
+}
+
+function ticketsToMap(tickets) {
+  const tickesMap = new Map();
+
+  tickets.forEach((el) => {
+    if (tickesMap.has(el[0])) {
+      const value = tickesMap.get(el[0]);
+      value.push(el[1]);
+      value.sort((a, b) => {
+        if (a < b) {
+          return -1;
+        }
+
+        return 1;
+      });
+      tickesMap.set(el[0], value);
+    } else {
+      tickesMap.set(el[0], [el[1]]);
+    }
+  });
+
+  return tickesMap;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function solution(tickets) {
   let answer = [];
   const tickesMap = new Map();
 
@@ -74,6 +176,10 @@ function findItenary(tickesMap, start, answer) {
 
   return answer;
 }
+*/
 
-console.log(solution(tickets1));
-console.log(solution(tickets2));
+console.log('answer : ',solution(tickets1));
+console.log('answer : ',solution(tickets2));
+console.log('answer : ',solution(tickets3));
+console.log('answer : ',solution(tickets4));
+console.log('answer : ',solution(tickets5));
